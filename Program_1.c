e93cb63c6a21f921245c91e0ac31d49b88c98123
Rifah A Khan <rifahakhan@gmail.com>
2021-02-19 14:45:27 +0000

    Program to find the number of vowels in a string.

--- Program_1.c
+++ Program_1.c
@@ -1,13 +1,23 @@
 //You will be given a string 'str'. Your task is to find the number of vowels in 'str' and print it
 //Input: loop Output: 2
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
 	char str[10];
+  int i,a=0,b=0;
+
+  printf("Enter a string\n");
 	scanf("%s", str);
 
-	//Write your code here
-
-	return 0;
+  for(i=0;i<strlen(str);i++)
+  {
+    if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+    a=a+1;
+    else
+    b=b+1;
+  }
+  printf("\n The no. of vovels are:%d",a);
+return 0;
 }
